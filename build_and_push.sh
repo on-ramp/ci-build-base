15c70fc774e9acdbccef181eac7690c2796c5fc9
Arnau Abella <arnauabella@gmail.com>
2019-09-03 12:16:57 +0200

    Fix openssl build

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]
   then
-    echo "Usage: ./build_and_push.sh version (e.g. 0.5.1)"
+    echo "Usage: ./build_and_push.sh version (e.g. 0.0.1)"
 else
   docker build -t coinweb/ci-build-base:$1 .
 	docker push coinweb/ci-build-base:$1
