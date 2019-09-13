946b69e5d82eb6d3b8a9647ce9351931d2774e18
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2019-09-13 16:31:44 +0200

    Adding ssh

--- install_deps.sh
+++ install_deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update -y && sudo apt-get install -y libpq-dev autoconf openssl autogen libtool make g++-7 libffi-dev libgmp-dev wget liblzma-dev liblzma5
+sudo apt-get update -y && sudo apt-get install -y libpq-dev autoconf openssl autogen libtool make g++-7 libffi-dev libgmp-dev wget liblzma-dev liblzma5 ssh
