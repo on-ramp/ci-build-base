9ca776749d015732326ad49df9649e5a88f43f4e
Constantine <penguinush@zoho.com>
2020-01-31 17:19:30 +0200

    Uncommenting docker push

--- build_and_push.sh
+++ build_and_push.sh
@@ -7,7 +7,7 @@ elif [[ $1 == "base-stack" ]] || [[ $1 == "ci-build-base" ]]
 then 
   echo "coinweb/$1:$2"
   docker build -t coinweb/$1:$2 -f Dockerfile.$1 --build-arg lts_version=$2 .
-	#docker push coinweb/$1:$2
+	docker push coinweb/$1:$2
 else
   echo "Name should be either base-stack or ci-build-base"
 fi
