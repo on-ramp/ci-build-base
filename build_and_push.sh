f7eb42eddc7feb5eb645e9fb1d65e2628493a33b
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-02-25 13:51:06 +0100

    Fixing ci base image

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,7 +4,7 @@ if [[ $# -lt 2 ]]
 then
   echo "Usage: ./build_and_push.sh name (base-stack or ci-build-base) version (e.g. lts-14.5)"
 elif [[ $1 == "base-stack" ]] || [[ $1 == "ci-build-base" ]]
-then 
+then
   echo "coinweb/$1:$2"
   docker build -t coinweb/$1:$2 -f Dockerfile.$1 --build-arg lts_version=$2 .
 	docker push coinweb/$1:$2
--- install_pgquarrel.sh
+++ install_pgquarrel.sh
@@ -2,11 +2,9 @@
 curl -L -o pgquarrel.tar.gz https://github.com/eulerto/pgquarrel/archive/pgquarrel_0_6_0.tar.gz
 mkdir pgquarrel && tar -xzf pgquarrel.tar.gz -C pgquarrel --strip-components 1
 cd pgquarrel
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local/pgquarrel -DCMAKE_PREFIX_PATH=/usr/lib/postgresql/12 .
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_PREFIX_PATH=/usr/lib/postgresql/12 .
 make
 make install
 
-echo 'export PATH=$PATH:/usr/local/pgquarrel/bin' >> ~/.bashrc 
-source ~/.bashrc
-
-ln -s /usr/local/pgquarrel/lib/libmini.so /usr/lib/libmini.so
+rm -rf /usr/lib/libmini.so
+ln -s /usr/local/lib/libmini.so /usr/lib/libmini.so
