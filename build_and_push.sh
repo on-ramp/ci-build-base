0a00d407eee80a36fc084565d4d71aede6e47898
Constantine <penguinush@zoho.com>
2022-10-14 09:54:43 +0300

    Adding alpine and ami2 images

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,8 +4,8 @@ usage () {
   echo "Usage: $0 NAME VERSION"
   echo ""
   echo "Arguments:"
-  echo "  NAME          base-stack|ci-build-base|stacktastic-build"
-  echo "  VERSION       target tag, e.g. lts-14.5"
+  echo "  NAME          circleci-image or base-stack|ci-build-base|stacktastic-build"
+  echo "  VERSION       target tag, e.g. lts-14.5; for circleci-image it should be one of alpine|ami2-dbmate"
 }
 
 [[ $# -eq 2 ]] || { usage; exit 1; }
@@ -13,15 +13,26 @@ usage () {
 ARG_NAME="$1"
 ARG_TAG="$2"
 
-if ! [[ "$ARG_NAME" =~ ^base-stack|ci-build-base|stacktastic-build$ ]]
+if [[ "$ARG_NAME" =~ ^circleci-image|base-stack|ci-build-base|stacktastic-build$ ]]
 then
+  if [[ "$ARG_NAME" == "circleci-image" && "$ARG_TAG" =~ ^alpine|ami2-dbmate$ ]]
+  then
+    IMGFULL="coinwebltd"
+  elif [[ "$ARG_NAME" != "circleci-image" ]]
+  then
+    IMGFULL="coinweb"
+  else
+    echo "Unrecognized tag"
+    exit 1
+  fi
+else
   echo "Unrecognized image flavor"
   exit 1
 fi
 
+[[ -n $IMGFULL ]] && IMGFULL="${IMGFULL}/${ARG_NAME}:${ARG_TAG}"
 #------------------------------------------------------------------------------#
 
-IMGFULL="coinweb/$ARG_NAME:$ARG_TAG"
 echo "Building $IMGFULL"
 set -x
 docker build \
